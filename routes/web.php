cdadfd2a637176ca78c6b6d9f84635ad35bc4f60
Andres Parra <aparra229@unab.edu.co>
2025-02-27 12:22:33 -0500

    rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo "Listado de productos";
+});
+
+Route::get('/products/create', function () {
+    echo "formulario para crear un producto";
+});
+
+Route::get('/products/{name}/{category}', function ($name,$category) {
+    echo "Producto:$name, y tiene una categoria $category";
+});
+
+
+
