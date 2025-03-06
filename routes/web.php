c5958c6678480dc5c5616a117fac13ce913d9087
Andres Parra <aparra229@unab.edu.co>
2025-03-06 12:07:47 -0500

    group routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Homecontroller::class);
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::get('/products/{name}', [ProductController::class, 'show']);
+Route::prefix('/prducts')->controller(ProductController::class)->group(function(){
+    Route::get('/', 'index');
+    Route::get('/create',  'create');
+    Route::get('/{name}','show');
+});
