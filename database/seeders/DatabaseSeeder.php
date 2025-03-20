5ff82ffa59c6061b3caf7c5cbd87902ded6ace4d
Andres Parra <aparra229@unab.edu.co>
2025-03-20 12:33:52 -0500

    creacion de factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class
         ); 
         
+
+        Category::factory(100)->create();
     }
 }
